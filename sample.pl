10934f93b038c2a6cde41b1b12bab98a83f0385d
Matt Lilley <thetrime@gmail.com>
2016-02-24 03:34:33 +1300

    Tidy

--- sample.pl
+++ sample.pl
@@ -1,20 +1,15 @@
-my_event_handler(_State, _Props, [something='Did this just change?']).
+my_event_handler(_State, _Props, [button_label='Did this just change?']).
 
 render_App(_, _, element('Panel',[layout=horiztonal, fill=horizontal], [element('Title',[label='This is a title from Prolog'],[]),
-                                                                        element('Splunge', [some_property='This is a property'], []),
+                                                                        element('SomeSubcomponent', [title_label='This is a property'], []),
                                                                         element('Field',[label='CP Code'],[]),
                                                                         element('Field',[label='CP Account ID'],[]),
                                                                         element('Button',[label='Submit'],[])])).
 
-render_SecondElement(_, _, element('Panel',[layout=horiztonal, fill=horizontal], [element('Title',[label='This is a totally different title!'],[]),
-                                                                                  element('Field',[label='CP Code'],[]),
-                                                                                  element('Field',[label='CP Account ID'],[]),
-                                                                                  element('Button',[label='Still Submit'],[])])).
-
-render_Splunge(State, Props, element('Panel', [layout=horizontal, fill=horizontal], [element('Title', [label=Label], []),
-                                                                                     element('Button', [label=Something, onClick=my_event_handler], [])])):-
-        memberchk(some_property=Label, Props),
-        memberchk(something=Something, State).
+render_SomeSubcomponent(State, Props, element('Panel', [layout=horizontal, fill=horizontal], [element('Title', [label=Label], []),
+                                                                                              element('Button', [label=ButtonLabel, onClick=my_event_handler], [])])):-
+        memberchk(title_label=Label, Props),
+        memberchk(button_label=ButtonLabel, State).
 
 
-getInitialState_Splunge([something='This is determined by state!']).
+getInitialState_SomeSubcomponent([button_label='This is determined by state!']).
