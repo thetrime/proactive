35f0361a7292cf4fc0b73b3503ca8d45e32a4bf3
Matt Lilley <thetrime@gmail.com>
2016-03-03 03:56:31 +1300

    Remove label from field again - the Field is meant to be just that

--- src/ui/Field.java
+++ src/ui/Field.java
@@ -14,40 +14,17 @@ import org.proactive.ReactLeafComponent;
 public class Field extends ReactLeafComponent 
 {
    private JTextField field = new JTextField();
-   private JPanel panel = new JPanel();
-   private JLabel label = null;
    public Field(PrologNode n, PrologContext context)
    {
       super(context);
-      panel.setLayout(new BorderLayout());
-      panel.add(field, BorderLayout.CENTER);
    }
    public void setProperty(String name, PrologObject value)
    {
       if (name.equals("fill"))
          fill = value.asFill();
-      if (name.equals("label"))
-      {
-         if (value == null)
-         {
-            if (label != null)
-               panel.remove(label);
-            label = null;
-         }
-         else
-         {
-            if (label == null)
-            {
-               label = new JLabel();
-               panel.add(label, BorderLayout.WEST);
-            }
-            label.setText(value.asString());
-         }
-      }
-
    }
    public Component getAWTComponent()
    {
-      return panel;
+      return field;
    }
 }
