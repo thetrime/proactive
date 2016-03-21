7e36b5693ac08247dc89e1012cef89bff235ee55
Matt Lilley <thetrime@gmail.com>
2016-03-21 23:18:16 +1300

    We always call this if we have a DESTROY vPatch but we should only act upon it if the target IS a widget

--- src/prolog/Predicate_destroy_widget.java
+++ src/prolog/Predicate_destroy_widget.java
@@ -13,8 +13,11 @@ public class Predicate_destroy_widget extends ExecuteOnlyCode
    public RC execute(Interpreter interpreter, boolean backtrackMode, gnu.prolog.term.Term args[]) throws PrologException
    {
       ReactComponent domNode = (ReactComponent)((JavaObjectTerm)args[0]).value;
-      ReactWidget widget = ((ReactWidget)domNode);
-      widget.destroy();
+      if (domNode instanceof ReactWidget)
+      {
+	 ReactWidget widget = ((ReactWidget)domNode);
+	 widget.destroy();
+      }
       return RC.SUCCESS_LAST;
    }
 }
