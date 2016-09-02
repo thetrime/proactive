89abfafd94ab469218b9d1fbbc396c09a5ee5120
Matt Lilley <thetrime@gmail.com>
2016-09-02 22:53:34 +1200

    Use a callback to ensure the system is ready before we start

--- src/proactive.js
+++ src/proactive.js
@@ -1,29 +1,34 @@
 "use strict";
-var PrologEngine = require('./prolog_engine');
-var ReactWidget = require('./react_widget');
-var PrologState = require('./prolog_state');
-var Constants = require('./constants');
-var Prolog = require('../lib/proscript2/build/proscript.js');
-var ReactComponent = require('./react_component.js');
-var ProactiveComponentFactory = require('./proactive_component_factory.js');
 
-function render(url, rootElementId, container, callback)
+window.onPrologReady = function(Prolog)
 {
-    var engine = new PrologEngine(url, rootElementId, function()
-                                  {
-                                      new ReactWidget(null, engine, rootElementId, PrologState.emptyState, function(widget)
-                                                      {
-                                                          container.className += " proactive_container vertical_layout vertical_fill horizontal_fill";
-                                                          container.appendChild(widget.getDOMNode());
-                                                          callback();
-                                                      })
-                                  });
+    var ProactiveComponentFactory = require('./proactive_component_factory.js');
+    var Constants = require('./constants');
+    var PrologEngine = require('./prolog_engine')
+    var ReactWidget = require('./react_widget');
+    var PrologState = require('./prolog_state');
+    var ReactComponent = require('./react_component.js');
+    if (onProactiveReady !== undefined)
+	onProactiveReady({render: function(url, rootElementId, container, callback)
+			  {
+			      var engine = new PrologEngine(url, rootElementId, function()
+							      {
+								  new ReactWidget(null, engine, rootElementId, PrologState.emptyState, function(widget)
+										    {
+											container.className += " proactive_container vertical_layout vertical_fill horizontal_fill";
+											container.appendChild(widget.getDOMNode());
+											callback();
+										    })
+							      });
+			  },
+			  registerComponent: ProactiveComponentFactory.registerComponent,
+			  ReactComponent: ReactComponent,
+			  Constants: Constants,
+			  make_atom: Prolog._make_atom}
+			);
 }
+console.log("Waiting for Prolog...");
+require('../lib/proscript2/build/proscript.js');
 
 
-module.exports = {render: render,
-                  registerComponent: ProactiveComponentFactory.registerComponent,
-                  ReactComponent: ReactComponent,
-                  Constants: Constants,
-                  make_atom: Prolog._make_atom,
-                  _qqqx: Prolog._qqq};
+module.exports = {};
