a222d1c66543dcf6748c562738c045fce452d196
Matt Lilley <thetrime@gmail.com>
2016-09-27 19:27:15 +1300

    Update test

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-//Proactive.render("http://localhost:10080/react", "Foo", document.getElementById("container"));
-Proactive.render("http://localhost:8080/react", "trade_entry_proactive", document.getElementById("container"), function() {});
+Proactive.render("http://192.168.0.13:8080/react", "trade_entry_proactive", document.getElementById("container"), function() {});
+//Proactive.render("http://localhost:8080/react", "foo", document.getElementById("container"), function() {});
+
 
