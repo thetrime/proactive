de99ad358349ae9d54ac83c90ae8cf9d5788361a
Matt Lilley <thetrime@gmail.com>
2016-10-25 21:56:51 +1300

    Preserve error number too

--- src/proactive.js
+++ src/proactive.js
@@ -11,17 +11,17 @@ window.onPrologReady = function(Prolog)
     if (onProactiveReady !== undefined)
 	onProactiveReady({render: function(url, rootElementId, container, callback)
 			  {
-                              var engine = new PrologEngine(url, rootElementId, function(status)
+                              var engine = new PrologEngine(url, rootElementId, function(status, error)
                                                             {
                                                                 if (status)
                                                                     new ReactWidget(null, engine, rootElementId, PrologState.emptyState, function(widget)
 										    {
 											container.className += " proactive_container vertical_layout vertical_fill horizontal_fill";
 											container.appendChild(widget.getDOMNode());
-                                                                                        callback(true);
+                                                                                        callback(true, error);
                                                                                     });
                                                                 else
-                                                                    callback(false);
+                                                                    callback(false, error);
 							      });
                           },
                           registerPredicate: Prolog._define_foreign,
