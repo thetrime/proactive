772a96eb8107444e18606043bc19884752197141
Matt Lilley <thetrime@gmail.com>
2016-12-06 22:17:20 +1300

    Add double click handler attribute onDblClick to Row

--- src/row.js
+++ src/row.js
@@ -1,4 +1,6 @@
 var ReactComponent = require('./react_component');
+var Prolog = require('proscript');
+var Constants = require('./constants.js');
 
 function Row()
 {
@@ -13,5 +15,32 @@ Row.prototype.appendChild = function(t)
     cell.appendChild(t.getDOMNode());
     this.domNode.appendChild(cell);
 }
+
+Row.prototype.setProperties = function(t)
+{
+   ReactComponent.prototype.setProperties.call(this, t);
+    if (t.onDblClick !== undefined)
+        this.setDblClickHandler(t.onDblClick);
+}
+
+Row.prototype.setDblClickHandler = function(value)
+{
+    if (this.dblClickHandler != null)
+        Prolog._free_local(this.dblClickHandler);
+    if (ReactComponent.isNull(value))
+    {
+        if (this.domNode.onDblClick !== undefined)
+            this.domNode.onDblClick = undefined;
+        return;
+    }
+    this.dblClickHandler = Prolog._make_local(value);
+    this.domNode.ondblclick = dblClickHandler.bind(this);
+}
+
+function dblClickHandler(event)
+{
+    this.getOwnerDocument().triggerEvent(this.dblClickHandler, Constants.emptyListAtom, function() {});
+}
+
 
 module.exports = Row;
