68820f5ab520d9f76b00be7f62aba6dfdcfb180e
Matt Lilley <thetrime@gmail.com>
2017-01-15 04:41:38 +1300

    Add SocketRocket

---
+++ src/CodeListener.h
@@ -0,0 +1,13 @@
+//
+//  CodeListener.h
+//  proactive
+//
+//  Created by Matt Lilley on 15/01/17.
+//  Copyright © 2017 Matt Lilley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CodeListener : NSObject
+
+@end
---
+++ src/CodeListener.m
@@ -0,0 +1,13 @@
+//
+//  CodeListener.m
+//  proactive
+//
+//  Created by Matt Lilley on 15/01/17.
+//  Copyright © 2017 Matt Lilley. All rights reserved.
+//
+
+#import "CodeListener.h"
+
+@implementation CodeListener
+
+@end
