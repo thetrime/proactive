be3ad3450633c59581d4764259041a62d75ea7a6
Matt Lilley <thetrime@gmail.com>
2017-01-31 23:06:57 +1300

    Remove cruft - this is now all inside Makefile

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=$(npm list --depth -1 | sed -e "s/proactive@\([^ ]*\) .*/\1/g" | head -1)
-mkdir proactivejs-${VERSION}
-browserify --standalone Proactive -t brfs src/core.js | uglifyjs -cm > proactivejs-${VERSION}/proactive.js
-cp node_modules/proscript/proscript.js.mem proactivejs-${VERSION}/
-cp proactive.css proactivejs-${VERSION}/
-zip -r proactivejs-${VERSION}.zip proactivejs-${VERSION}/
-#rm -rf proactivejs-${VERSION}
