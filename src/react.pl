9c04b8173be1532ece0a0ba130a3839f5880e484
Matt Lilley <thetrime@gmail.com>
2017-04-04 17:11:11 +1200

    Turns out that was not such a great idea. It imposes an ordering dependency on the load

--- src/react.pl
+++ src/react.pl
@@ -369,8 +369,7 @@ user:on_server(X):- X.
 raise_event(Key, _):- permission_error(raise, server_side_event, Key).
 wait_for(List):- permission_error(wait_for, server_side_event, List).
 
-user:term_expansion(requires(X), [depends_on(X), :-use_module(Filename)]):-
-    module_property(X, file(Filename)).
+user:term_expansion(requires(X), [depends_on(X), :-use_module(X)]).
 
 vdiff_warning(X):-
         format(user_error, 'Warning: ~w', [X]).
