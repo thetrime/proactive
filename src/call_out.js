2a42842b8b6bd68ca00c8723f5c893be3e4aecfe
Matt Lilley <thetrime@gmail.com>
2017-12-08 00:46:02 +1300

    Redesign call-out a bit

--- src/call_out.js
+++ src/call_out.js
@@ -17,23 +17,25 @@ CallOut.prototype = new Panel;
 CallOut.prototype.setProperties = function(t)
 {
     Panel.prototype.setProperties.call(this, t);
-}
-
-CallOut.prototype.setParent = function(t)
-{
-    ReactComponent.prototype.setParent.call(this, t);
-    if (t !== null)
+    if (t['for'] != undefined)
+    {
+        var target_id = Prolog._portray(t['for']);
         window.setTimeout(function()
                           {
-                              var rect = t.getDOMNode().getBoundingClientRect();
+                              var f = document.getElementById(target_id);
+                              if (f == null)
+                                  return;
+                              var rect = f.getBoundingClientRect();
                               // This is the visible offset from the nearest positioned ancestor
-                              var ancestorRect = t.getDOMNode().offsetParent.getBoundingClientRect();
+                              var ancestorRect = f.offsetParent.getBoundingClientRect();
                               // This is still not exactly right - if the parent is itself positioned, we have a problem
-                              this.getDOMNode().style.top = (rect.top + t.getDOMNode().clientHeight + 5 - ancestorRect.top) + 'px';
+                              this.getDOMNode().style.top = (rect.top + f.clientHeight + 5 - ancestorRect.top + 10) + 'px';
                               this.getDOMNode().style.left = rect.left + (rect.width/2) - (this.getDOMNode().clientWidth/2) + 'px';
                               this.getDOMNode().style.visibility = 'visible';
+                              this.getDOMNode().style.opacity = '1';
 
                           }.bind(this), 0);
+    }
 }
 
 module.exports = CallOut;
