47b10ced6c6511df2f08c2586a35dcd2a3655c8a
Matt Lilley <thetrime@gmail.com>
2017-12-19 00:59:17 +1300

    Implement replaceChild, insertBefore and removeChild in TableFooter

--- src/table_footer.js
+++ src/table_footer.js
@@ -4,7 +4,6 @@ function TableFooter()
 {
     ReactComponent.call(this);
     this.setDOMNode(document.createElement("tr"));
-    this.elements = [];
 }
 TableFooter.prototype = new ReactComponent;
 
@@ -13,13 +12,39 @@ TableFooter.prototype.appendChild = function(t)
     var cell = document.createElement("td");
     cell.appendChild(t.getDOMNode());
     this.domNode.appendChild(cell);
-    this.elements.push(cell);
     t.setParent(this);
 }
 
-TableFooter.prototype.getElements = function()
+TableFooter.prototype.removeChild = function(t)
 {
-    return this.elements;
+    var index = this.children.indexOf(t);
+    this.children.splice(index, 1);
+    t.setParent(null);
+    this.domNode.removeChild(this.domNode.childNodes[index]);
+    if (this.parent != null)
+        this.parent.markDirty();
 }
 
+TableFooter.prototype.insertBefore = function(t, s)
+{
+    var cell = document.createElement("td");
+    cell.appendChild(t.domNode);
+    var index = this.children.indexOf(s);
+    this.domNode.insertBefore(cell, s.getDOMNode().parentNode);
+    t.setParent(this);
+    if (this.parent != null)
+        this.parent.markDirty();
+}
+
+TableFooter.prototype.replaceChild = function(n, o)
+{
+    var cell = document.createElement("td");
+    cell.appendChild(t.domNode);
+    var index = this.children.indexOf(s);
+    this.domNode.replaceChild(cell, o.getDOMNode().parentNode);
+    n.setParent(this);
+    o.setParent(null);
+    if (this.parent != null)
+        this.parent.markDirty();
+}
 module.exports = TableFooter;
