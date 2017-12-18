bffa37d9b583bba4baeed9bdf27e35b1baa5eebe
Matt Lilley <thetrime@gmail.com>
2017-12-19 00:49:49 +1300

    Implement replaceChild, insertBefore and removeChild in TableHeader

--- src/table_header.js
+++ src/table_header.js
@@ -5,36 +5,70 @@ var default_weight = 1;
 function TableHeader()
 {
     ReactComponent.call(this);
-    this.weights = [];
     this.tr = document.createElement("tr");
     this.setDOMNode(this.tr);
-    this.sum = 0;
-    this.elements = [];
 }
 
 TableHeader.prototype = new ReactComponent;
 
 
-// This is tricky since t.weight may be undefined, a number or an atom like "40%"
-// When adjusting this.sum, we must treat 40% as default_weight, but when updating weights[] we must leave it as 40%
-// That is why it looks like we calculate the same value a lot: new_value is NOT the same as n.value || default_value!
 TableHeader.prototype.appendChild = function(t)
 {
     var cell = document.createElement("th");
     cell.className = "react_table_header_cell";
     cell.appendChild(t.domNode);
-    this.elements.push(cell);
     this.getDOMNode().appendChild(cell);
-    this.weights[this.children.indexOf(t)] = t.weight || default_weight;
-    this.sum += (Number(t.weight) || default_weight);
     t.setParent(this);
-    // FIXME: Notify parent that it may need to relayout
+    if (this.parent != null)
+    {
+        this.parent.column_count++;
+        this.parent.markDirty();
+    }
 }
 
-TableHeader.prototype.getElements = function()
+
+TableHeader.prototype.removeChild = function(t)
 {
-    return this.elements;
+    var index = this.children.indexOf(t);
+    this.children.splice(index, 1);
+    t.setParent(null);
+    this.tr.removeChild(this.tr.childNodes[index]);
+    if (this.parent != null)
+    {
+        this.parent.column_count--;
+        this.parent.markDirty();
+    }
 }
-// FIXME: Implement other methods
+
+TableHeader.prototype.insertBefore = function(t, s)
+{
+    var cell = document.createElement("th");
+    cell.className = "react_table_header_cell";
+    cell.appendChild(t.domNode);
+    var index = this.children.indexOf(s);
+    this.tr.insertBefore(cell, s.getDOMNode().parentNode);
+    t.setParent(this);
+    if (this.parent != null)
+    {
+        this.parent.column_count++;
+        this.parent.markDirty();
+    }
+}
+
+TableHeader.prototype.replaceChild = function(n, o)
+{
+    var cell = document.createElement("th");
+    cell.className = "react_table_header_cell";
+    cell.appendChild(t.domNode);
+    var index = this.children.indexOf(s);
+    this.domNode.replaceChild(cell, o.getDOMNode().parentNode);
+    n.setParent(this);
+    o.setParent(null);
+    if (this.parent != null)
+    {
+        this.parent.column_count++;
+        this.parent.markDirty();
+    }
+}
 
 module.exports = TableHeader;
